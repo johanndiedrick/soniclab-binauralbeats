ca298c9bed7d096e05de7a700f6f07bcc97f9c7d
Johann Diedrick <jdiedrick@gmail.com>
2015-03-13 15:46:35 -0400

    final draft of presentation

--- js/script.js
+++ js/script.js
@@ -26,5 +26,70 @@ $("document").ready(function(){
 		return false;
 	});
 
+	$('a#binaural-beats-link').click(function(){
+	
+		$('html, body').animate({
+			scrollTop: $("#youtube-explaination").offset().top
+		}, 2000);
+		return false;	
+	});
 
+	
+	$('a#youtube-explaination-link').click(function(){
+	
+		$('html, body').animate({
+			scrollTop: $("#question").offset().top
+		}, 2000);
+		return false;	
+	});
+	
+	$('a#question-link').click(function(){
+		
+		$('html, body').animate({
+			scrollTop: $("div#programming").offset().top
+		}, 2000);
+		return false;
+	});
+	$('a#programming-link').click(function(){
+	
+		$('html, body').animate({
+			scrollTop: $("#coreaudio").offset().top
+		}, 2000);
+		return false;	
+	});
+
+	
+	$('a#coreaudio-link').click(function(){
+	
+		$('html, body').animate({
+			scrollTop: $("#audiounits").offset().top
+		}, 2000);
+		return false;	
+	});
+	
+	$('a#audiounits-link').click(function(){
+		
+		$('html, body').animate({
+			scrollTop: $("div#start").offset().top
+		}, 2000);
+		return false;
+	});
+
+	$('a#start-link').click(function(){
+	
+		$('html, body').animate({
+			scrollTop: $("#references").offset().top
+		}, 2000);
+		return false;	
+	});
+
+	
+	$('a#references-link').click(function(){
+	
+		$('html, body').animate({
+			scrollTop: $("#thank-you").offset().top
+		}, 2000);
+		return false;	
+	});
+	
 });
